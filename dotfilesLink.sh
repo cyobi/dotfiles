cf72be8c473a912cc4b65e21d7b411684a00d8eb
cyobi <kotaro.db@gmail.com>
2016-03-25 15:16:25 +0900

    test

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshenv ~/.zshenv
 
